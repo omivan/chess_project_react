e98c5954eb23c8049317a49d6a4acdc0c3f4b699
ivan.omelchenk <ivan.omelchenko@paydock.com>
2024-05-29 20:09:56 +0100

    chess board visulize

--- src/Components/ChessGame.js
+++ src/Components/ChessGame.js
@@ -7,44 +7,68 @@ class ChessGame extends React.Component {
         super(props);
         this.state = {
             chess: new Chess(),
-            fen: 'start', // Starting FEN notation of the board
-            gameOver: false // State to track if the game is over
+            fen: 'start',
+            gameOver: false,
+            squareStyles: {},
+            selectedSquare: '',
+            moves: []
         };
+
+        this.boardRef = React.createRef();
     }
 
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside);
+    }
+
+    handleClickOutside = (event) => {
+        if (this.boardRef && !this.boardRef.current.contains(event.target)) {
+            this.clearHighlights();
+        }
+    };
+
+    clearHighlights = () => {
+        if (Object.keys(this.state.squareStyles).length > 0) {
+            this.setState({ squareStyles: {}, selectedSquare: '' });
+        }
+    };
+
     onDrop = ({ sourceSquare, targetSquare }) => {
-        // Create a temporary instance to move without changing state
+        this.makeMove(sourceSquare, targetSquare);
+    };
+
+    makeMove = (sourceSquare, targetSquare) => {
         let tempChess = new Chess(this.state.chess.fen());
-
-
-        // Get all legal moves from the source square
         const legalMoves = tempChess.moves({ square: sourceSquare, verbose: true });
-
-        // Check if the intended move is in the list of legal moves
         const isMoveLegal = legalMoves.some(move => move.to === targetSquare);
 
         if (!isMoveLegal) {
-            return; // Exit if the move is not legal
+            return;
         }
 
-        // Attempt to make a move
         let move = tempChess.move({
             from: sourceSquare,
             to: targetSquare,
-            promotion: 'q' // Promote to a queen if applicable
+            promotion: 'q'
         });
-        console.log(move)
 
-        if (move === null) return; // Ignore if move is illegal
+        if (move === null) return;
 
-        // Update the state with the new valid move
-        this.setState({ chess: tempChess, fen: tempChess.fen() }, () => {
-            // Check for end of game
+        this.setState(prevState => ({
+            chess: tempChess,
+            fen: tempChess.fen(),
+            squareStyles: {},
+            selectedSquare: '',
+            moves: [...prevState.moves, move.san]
+        }), () => {
             if (tempChess.isGameOver()) {
                 this.setState({ gameOver: true });
                 alert('Game over');
             } else {
-                // Make a move for the bot
                 this.makeRandomMove();
             }
         });
@@ -52,7 +76,6 @@ class ChessGame extends React.Component {
 
     makeRandomMove = () => {
         let possibleMoves = this.state.chess.moves();
-
         if (possibleMoves.length === 0) {
             this.setState({ gameOver: true });
             alert('Game over');
@@ -60,19 +83,70 @@ class ChessGame extends React.Component {
         }
 
         let randomIdx = Math.floor(Math.random() * possibleMoves.length);
-        this.state.chess.move(possibleMoves[randomIdx]);
-        this.setState({ chess: this.state.chess, fen: this.state.chess.fen() });
+        let move = this.state.chess.move(possibleMoves[randomIdx]);
+        this.setState(prevState => ({
+            chess: this.state.chess,
+            fen: this.state.chess.fen(),
+            squareStyles: {},
+            moves: [...prevState.moves, move.san]
+        }));
+    };
+
+    handleSquareClick = (square) => {
+        const { chess, selectedSquare } = this.state;
+
+        if (selectedSquare && selectedSquare !== square) {
+            this.makeMove(selectedSquare, square);
+        } else {
+            const moves = chess.moves({ square, verbose: true });
+
+            if (moves.length === 0 || selectedSquare === square) {
+                this.clearHighlights();
+            } else {
+                const newSquareStyles = moves.reduce((styles, move) => ({
+                    ...styles,
+                    [move.to]: {
+                        background: "rgba(255, 255, 100, 0.5)"
+                    }
+                }), {});
+
+                this.setState({ squareStyles: newSquareStyles, selectedSquare: square });
+            }
+        }
     };
 
     render() {
+        const movePairs = [];
+        for (let i = 0; i < this.state.moves.length; i += 2) {
+            movePairs.push([this.state.moves[i], this.state.moves[i + 1]]);
+        }
+
         return (
-            <div>
-                <Chessboard
-                    position={this.state.fen}
-                    onDrop={this.onDrop}
-                    width={320}
-                />
-                {this.state.gameOver && <p>Game Over. Refresh to play again!</p>}
+            <div style={{ display: 'flex' }}>
+                <div ref={this.boardRef} style={{ position: "relative" }}>
+                    <Chessboard
+                        position={this.state.fen}
+                        onDrop={this.onDrop}
+                        width={800}
+                        squareStyles={this.state.squareStyles}
+                        onSquareClick={this.handleSquareClick}
+                    />
+                    {this.state.gameOver && <p className="game-over">Game Over. Refresh to play again!</p>}
+                </div>
+                <div style={{ marginLeft: '20px', width: '200px', overflowY: 'auto', maxHeight: '800px' }}>
+                    <h3>Moves</h3>
+                    <table>
+                        <tbody>
+                        {movePairs.map((pair, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}.</td>
+                                <td>{pair[0]}</td>
+                                <td>{pair[1]}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
